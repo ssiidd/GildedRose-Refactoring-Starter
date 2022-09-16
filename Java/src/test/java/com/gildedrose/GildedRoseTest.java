e808ac93416376d8c0239e029aa2b1d86a0e417c
Siddharth <spatel@lsoc.org>
2022-09-16 08:56:51 -0500

    added some tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 4) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(5, app.items[0].quality);
     }
+    void testBrie2() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+    void testVest() {
+        Item[] items = new Item[] { new Item("Vest", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+    void testVest2() {
+        Item[] items = new Item[] { new Item("Vest", -1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+    void testSulfuras() {
+        Item[] items = new Item[] { new Item("Sulfuras", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(5, app.items[0].quality);
+    }
+    
 
 }
